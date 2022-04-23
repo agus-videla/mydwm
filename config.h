e62d400d7270f918b52cfaa742a1c65ad424d9bd
Agustín Octavio Videla <agustinvidela0198@gmail.com>
2022-04-23 13:42:23 -0300

    applied status2d patch (kinda works but crashes)

--- config.h
+++ config.h
@@ -1,4 +1,5 @@
 /* See LICENSE file for copyright and license details. */
+#define TERMINAL "alacritty"
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
@@ -45,12 +46,12 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod4Mask
+#define SUPER Mod4Mask
 #define TAGKEYS(KEY,TAG) \
-	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
-	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
+	{ SUPER,                       KEY,      view,           {.ui = 1 << TAG} }, \
+	{ SUPER|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
+	{ SUPER|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
+	{ SUPER|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
@@ -62,30 +63,36 @@ static const char *termcmd[]     = { "alacritty", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_r,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_w,      spawn,          SHCMD("$BROWSER") },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY,                       XK_q,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ SUPER,                       XK_r,      spawn,          {.v = dmenucmd } },
+	{ SUPER,                       XK_Return, spawn,          {.v = termcmd } },
+	{ SUPER,                       XK_w,      spawn,          SHCMD("$BROWSER") },
+	{ SUPER,                       XK_b,      togglebar,      {0} },
+	{ SUPER,                       XK_j,      focusstack,     {.i = +1 } },
+	{ SUPER,                       XK_k,      focusstack,     {.i = -1 } },
+	{ SUPER,                       XK_i,      incnmaster,     {.i = +1 } },
+	{ SUPER,                       XK_d,      incnmaster,     {.i = -1 } },
+	{ SUPER,                       XK_h,      setmfact,       {.f = -0.05} },
+	{ SUPER,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ SUPER,                       XK_m,               spawn,          SHCMD(TERMINAL" -e ncmpcpp") },
+
+    //media
+	{ SUPER,                       XK_minus,           spawn,          SHCMD("pactl set-sink-volume @DEFAULT_SINK@ -2500 && pkill -RTMIN+10 dwmblocks") },
+	{ SUPER,                       XK_equal,           spawn,          SHCMD("pactl set-sink-volume @DEFAULT_SINK@ +2500 && pkill -RTMIN+10 dwmblocks") },
+	{ SUPER|ShiftMask,             XK_equal,           spawn,          SHCMD("pactl set-sink-mute @DEFAULT_SINK@ toggle && pkill -RTMIN+10 dwmblocks") },
+	{ SUPER,                       XK_p,               spawn,          SHCMD("mpc toggle") },
+	{ SUPER,                       XK_comma,           spawn,          SHCMD("mpc prev") },
+	{ SUPER,                       XK_period,          spawn,          SHCMD("mpc next") },
+
+	{ SUPER,                       XK_Return, zoom,           {0} },
+	{ SUPER,                       XK_Tab,    view,           {0} },
+	{ SUPER,                       XK_q,      killclient,     {0} },
+	{ SUPER,                       XK_t,      setlayout,      {.v = &layouts[0]} },
+	{ SUPER,                       XK_f,      setlayout,      {.v = &layouts[1]} },
+	{ SUPER,                       XK_u,      setlayout,      {.v = &layouts[2]} },
+	{ SUPER,                       XK_space,  setlayout,      {0} },
+	{ SUPER|ShiftMask,             XK_space,  togglefloating, {0} },
+	{ SUPER,                       XK_0,      view,           {.ui = ~0 } },
+	{ SUPER|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
@@ -94,8 +101,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_6,                      5) TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
-	{ MODKEY|ControlMask|ShiftMask, XK_q,      quit,           {1} }, 
+	{ SUPER|ShiftMask, XK_q,      quit,           {1} }, 
 };
 
 /* button definitions */
@@ -106,12 +112,12 @@ static Button buttons[] = {
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
 	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
-	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
-	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
-	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
+	{ ClkClientWin,         SUPER,         Button1,        movemouse,      {0} },
+	{ ClkClientWin,         SUPER,         Button2,        togglefloating, {0} },
+	{ ClkClientWin,         SUPER,         Button3,        resizemouse,    {0} },
 	{ ClkTagBar,            0,              Button1,        view,           {0} },
 	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
-	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
-	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+	{ ClkTagBar,            SUPER,         Button1,        tag,            {0} },
+	{ ClkTagBar,            SUPER,         Button3,        toggletag,      {0} },
 };
 
